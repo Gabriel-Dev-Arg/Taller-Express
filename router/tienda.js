911aa0f9a648062e472815eadf8078a5f61ea39b
gabriel <gabriel199777@outlook.com>
2024-10-15 23:00:51 -0300

    Agregamos 2 Endpoint a Tienda

--- router/tienda.js
+++ router/tienda.js
@@ -22,4 +22,40 @@ router.get("/",
 
 )
 
+//Endpoint para obtener los nombres de las tiendas
+//localhost:8080/api/tiendas/nombre/Distribuidora Sur
+router.get("/nombre/:nombre", async (req,res) => {
+    const { nombre } = req.params;
+    try {
+        let tienda = await Tienda.find( {nombre} )
+        return res.status(200).json(
+            {
+                response: tienda
+            }
+        )
+    } catch (error) {
+        return res.status(500).json({
+            response: error
+        });
+    }
+
+})
+//Endpoint para obtener las direcciones
+//localhost:8080/api/tiendas/direccion/Calle Falsa 123, Bogotá
+router.get("/direccion/:direccion", async (req,res) =>{
+    const {direccion} = req.params
+    try {
+        let tienda = await Tienda.find({direccion})
+        return res.status(200).json(
+            {
+                response: tienda
+            }
+        )
+    } catch (error) {
+        return res.status(500).json({
+            response: error
+        });
+    }
+})
+
 export default router
